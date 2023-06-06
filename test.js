666b163cb07a08c68506210dfd4d1765a0521b30
TermOfficial <79934414+TermOfficial@users.noreply.github.com>
2023-06-06 18:34:59 -0400

    min max

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-// test??
+// // Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+// Examples (Input --> Output)
+// [1,2,3,4,5] --> [1,5]
+// [2334454,5] --> [5,2334454]
+// [1]         --> [1,1]
+function minMax(arr) {
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+
+    return [min, max];
+}
+
+// Testing the function
+let numbers = [1, 2, 3, 4, 5];
+let result = minMax(numbers);
+console.log(`Min: ${result[0]}, Max: ${result[1]}`);  // Output: Min: 1, Max: 5
